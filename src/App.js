a8687e27946ed53912bcff61a2f1d10db2f1f2d8
MasonTDaniel <danielm@southwestern.edu>
2019-12-09 15:42:05 -0600

    Add search bar and APP ID and Key to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { directive } from "@babel/types";
 
-function App() {
+const App = () => {
+  const APP_ID = "92c867da";
+  const APP_KEY = "430b2e13b5f74eeda561b7536e06b5e6";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
