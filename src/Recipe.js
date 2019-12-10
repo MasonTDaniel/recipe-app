2463e35ddbbeb6d76decda3561bf10a30b352f28
MasonTDaniel <danielm@southwestern.edu>
2019-12-09 21:48:47 -0600

    Added styling for search result modules

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,21 @@
 import React from "react";
+import "./recipe-module.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div className="recipe">
       <h1>{title}</h1>
-      <ol>
-        {ingredients.map(ingredient => (
-          <li>{ingredient.text}</li>
-        ))}
-      </ol>
+      <div className="ingredients">
+        Ingredients
+        <ol className="ingredient-list">
+          {ingredients.map(ingredient => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ol>
+      </div>
+
       <p>{calories}</p>
-      <img src={image} alt="" />
+      <img className="image" src={image} alt="" />
     </div>
   );
 };
